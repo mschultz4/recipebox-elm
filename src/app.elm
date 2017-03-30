b80a7728e9891f602a599608868f9bf677431d4f
Matthew Schultz <mschultz4@gmail.com>
2017-03-29 22:06:51 -0400

    started dev: have an input box

--- src/app.elm
+++ src/app.elm
@@ -1,65 +1,86 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
 
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = newRecipe, view = view, update = update }
+
 
 
 -- MODEL
 
-type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+
+type alias Recipes =
+    { recipes : List Recipe
+    , visibility : String
+    , uid : Int
+    }
 
 
-model : Model
-model =
-  Model "" "" ""
+type alias Recipe =
+    { name : String
+    , ingredients : List String
+    , instructions : List String
+    , notes : String
+    , favorite : Bool
+    , rid : Int
+    }
+
+emptyRecipes : Recipes
+emptyRecipes = 
+    { recipes = []
+    , visibility = "ALL"
+    , uid  = 0
+    }
+
+
+newRecipe : Recipe
+newRecipe = 
+    { name = ""
+    , ingredients = []
+    , instructions = []
+    , notes = ""
+    , favorite = False
+    , rid = 0
+    }
+
 
 
 -- UPDATE
 
+
 type Msg
-    = Name String
-    | Password String
-    | PasswordAgain String
+    = Add String
+    | ToggleFavorite
 
 
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+update : Msg -> Recipe -> Recipe
+update msg model=
+    case msg of
+        Add name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        ToggleFavorite ->
+            { model 
+                | favorite = 
+                    if model.favorite then
+                        False
+                    else 
+                        True
+            }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
 
 
 -- VIEW
 
-view : Model -> Html Msg
-view model =
-  div []
-    [ input [ type_ "text", placeholder "Name", onInput Name ] []
-    , input [ type_ "password", placeholder "Password", onInput Password ] []
-    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
-    ]
 
-viewValidation : Model -> Html msg
-viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+view : Recipe -> Html Msg
+view model =
+    section []
+        [ h5 [] [ text "Recipe List" ]
+        , input [ type_ "text", placeholder "Name", onInput Add ] []
+        , div [] [text (toString model.name)]
+        ]
